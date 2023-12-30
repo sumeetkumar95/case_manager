eb592aadfda530971e9f1f44c24b0071abb92acb
sumeetkumar95 <sumeetraja027@gmail.com>
2023-12-30 18:46:58 +0500

    changings in floatButton

--- lib/screens/witnessmanager.dart
+++ lib/screens/witnessmanager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class WitnessManager extends StatefulWidget {
   const WitnessManager({super.key});
@@ -14,7 +15,63 @@ class _WitnessManagerState extends State<WitnessManager> {
       appBar: AppBar(
         title: Text("Witness Manager"),
         backgroundColor: Colors.deepPurple,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => _buildPopupDialog(context),
+          );
+        },
+        child: FaIcon(FontAwesomeIcons.plus),
       ),
     );
   }
+}
+
+Widget _buildPopupDialog(BuildContext context) {
+  return new AlertDialog(
+    title: const Text('Hearing Form'),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        TextFormField(
+          decoration: InputDecoration(
+              labelText: "Hearing Type",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+              labelText: "Judge Name",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+              labelText: "Hearing Venue",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+        )
+      ],
+    ),
+    actions: <Widget>[
+      Center(
+        child: new TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Save'),
+        ),
+      ),
+    ],
+  );
 }
